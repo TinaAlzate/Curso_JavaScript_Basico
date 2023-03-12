3c1a05511ef2c1fe01cca19aefab14d494857324
TinaAlzate <valzate618@gmail.com>
2023-03-12 13:09:50 +0100

    Tarea1-JavaScript-Node

---
+++ mi-primer-proyecto-node/index.js
@@ -0,0 +1,8 @@
+//  Este es el mensaje de bienvenida 
+console.log("Hola mundo");
+
+/*Esta es la parte intermedia de nuestro poryecto 
+En esta parte haremos chequeos intermedios*/
+
+// Este va a hacer el mensaje que se envia para despedir
+console.log("Adiós");
